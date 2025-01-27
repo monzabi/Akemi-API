79ce39e4a2e03272d181f619b6c3cb27e9858eb4
monzabi <extsayzz@gmail.com>
2025-01-27 23:33:20 +0100

    [+] .

--- src/main/java/org/akemi/api/AkemiAPILoader.java
+++ src/main/java/org/akemi/api/AkemiAPILoader.java
@@ -1,24 +1,25 @@
 package org.akemi.api;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.akemi.api.bukkit.packet.PacketName;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AkemiAPILoader extends JavaPlugin {
 
     @Getter
+    @Setter
     private static AkemiAPILoader API_LOADER;
     @Getter
     private PacketName packetName;
 
     @Override
     public void onEnable() {
-        API_LOADER = this;
     }
 
     @Override
     public void onDisable() {
-        API_LOADER = null;
+
     }
 
 
